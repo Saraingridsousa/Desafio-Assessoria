dad04f27ee68432fe20792b9fac7a118c31dbb6b
SaraIngrid <saraingrid2505@gmail.com>
2023-09-06 22:23:51 -0300

    Alteração no footer e na sessão de texto da página home

--- desafio-assessoria/src/components/Footer/index.tsx
+++ desafio-assessoria/src/components/Footer/index.tsx
@@ -4,32 +4,37 @@ import { GeneralText } from "../Text";
 
 export function Footer(){
     return(
-        <HStack
-        bg={"lightpink"}
-        py={9}
-        bottom={0}
-        position={"relative"}
-        
-        >
-            <Stack
-            left={0}
-            position={"absolute"}
-            >
-            <Image src={"/Logo.png"} alt={"Logo"} width={120} height={85}/>
-            </Stack>
+        <Stack>
+            <HStack
+            bg={"lightpink"}
+            py={9}
+            bottom={0}
+            position={"relative"}
             
-            <Stack
-            width={"100%"}
-            textAlign={"center"}
             >
-                <GeneralText text={"Vestindo sonhos, criando memórias, na La Belle Robe"} color={"darkpink"} fontSize={"20px"}/>
-            </Stack>
+                <Stack
+                left={0}
+                position={"absolute"}
+                >
+                <Image src={"/Logo.png"} alt={"Logo"} width={120} height={85}/>
+                </Stack>
+                
+                <Stack
+                width={"100%"}
+                textAlign={"center"}
+                >
+                    <Stack>
+                    <GeneralText text={"Vestindo sonhos, criando memórias, na La Belle Robe"} color={"darkpink"} fontSize={"20px"}/>
+                    </Stack>
 
-            <Stack
-            position={"absolute"}
-            right={2}>
-                <Image src={"/SocialFooter.png"} alt={"Redes Sociais"} width={170} height={50}/>
-            </Stack>
-        </HStack>
+                </Stack>
+
+                <Stack
+                position={"absolute"}
+                right={2}>
+                    <Image src={"/SocialFooter.png"} alt={"Redes Sociais"} width={170} height={50}/>
+                </Stack>
+            </HStack>
+        </Stack>
     )
 }
--- desafio-assessoria/src/components/Sections/index.tsx
+++ desafio-assessoria/src/components/Sections/index.tsx
@@ -17,6 +17,8 @@ export function TextHome(){
     <Stack
     marginTop={20}
     marginBottom={20}
+    marginRight={200} 
+    marginLeft={200}
     textAlign={"center"}
     >
         <GeneralText text={"Bem-vindo à La Belle Robe, onde cada vestido é mais do que apenas uma peça de roupa - é um portal para a celebração de momentos inesquecíveis. Acreditamos que as memórias de momentos preciosos merecem ser vividas de maneira extraordinária, e é por isso que cada vestido em nossa coleção é cuidadosamente escolhido para capturar a essência da elegância e da beleza."} color={"darkpink"} fontSize={"20px"}/>
