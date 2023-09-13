c3a9f6927d3237297f34972b3c67f07add05ca0d
SaraIngrid <saraingrid2505@gmail.com>
2023-09-13 00:42:20 -0300

    Estilização da tabela e atualização da página de membros

--- desafio-assessoria/src/components/Text/index.tsx
+++ desafio-assessoria/src/components/Text/index.tsx
@@ -5,14 +5,18 @@ interface GeneralTextProps{
     color: string
     fontSize: string
     textAlign?: any
+    fontWeight?: string
+    textTransform?: any
 }
 
-export function GeneralText({text, color,fontSize,textAlign}:GeneralTextProps){
+export function GeneralText({text, color,fontSize,textAlign,fontWeight,textTransform}:GeneralTextProps){
     return(
         <Text
         fontSize={fontSize}
         color={color}
         textAlign={textAlign}
+        fontWeight={fontWeight}
+        textTransform={textTransform}
         >
             {text}
         </Text>
--- desafio-assessoria/src/pages/membros.tsx
+++ desafio-assessoria/src/pages/membros.tsx
@@ -1,5 +1,6 @@
+import { GeneralText } from "@/components/Text";
 import { GeneralTitle } from "@/components/Title";
-import { HStack, Stack, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { HStack, Stack, Table, Thead, Tbody, Tr, Th, Td, Box } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -7,9 +8,8 @@ export default function Membros() {
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
-   
     axios
-      .get("http://localhost:3001/membrosj") 
+      .get("http://localhost:3001/membrosj")
       .then((response) => {
         setMembers(response.data);
       })
@@ -20,31 +20,54 @@ export default function Membros() {
 
   return (
     <>
-      <HStack paddingTop={"60px"} paddingBottom={"80px"} display={"flex"} justifyContent={"center"}>
+      <HStack paddingTop={"60px"} paddingBottom={"20px"} display={"flex"} justifyContent={"center"}>
         <Stack>
           <GeneralTitle text={"Membros"} color={"#fff"} fontSize={"24px"} bColor={"darkpink"} textAlign={"center"} />
         </Stack>
       </HStack>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Nome</Th>
-            <Th>E-mail</Th>
-            <Th>Aniversário</Th>
-            <Th>Cargo</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {members.map((member) => (
-            <Tr key={member.id}>
-              <Td>{member.name}</Td>
-              <Td>{member.email}</Td>
-              <Td>{member.birthday}</Td>
-              <Td>{member.position}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+
+      <Stack
+       marginBottom={20}
+        >
+        <HStack bg={"darkpink"}  height={"45px"} borderRadius={"16px 16px 0 0"}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>
+                  <GeneralText text={"Nome"} color={"white"} fontSize={"20px"} fontWeight={"normal"} textTransform={"none"} />
+                </Th>
+                <Th>
+                  <GeneralText text={"E-mail"} color={"white"} fontSize={"20px"} fontWeight={"normal"} textTransform={"none"} />
+                </Th>
+                <Th>
+                  <GeneralText text={"Aniversário"} color={"white"} fontSize={"20px"} fontWeight={"normal"} textTransform={"none"} />
+                </Th>
+                <Th>
+                  <GeneralText text={"Cargo"} color={"white"} fontSize={"20px"} fontWeight={"normal"} textTransform={"none"} />
+                </Th>
+              </Tr>
+            </Thead>
+          </Table>
+        </HStack>
+        <HStack
+          bg={"white"}
+          boxShadow={"0px 7px 4px 0px rgba(0, 0, 0, 0.20)"}
+          borderRadius={"0 0 16px 16px"}
+        >
+          <Table variant="simple">
+            <Tbody>
+              {members.map((member) => (
+                <Tr key={member.id}>
+                  <Td>{member.name}</Td>
+                  <Td>{member.email}</Td>
+                  <Td>{member.birthday}</Td>
+                  <Td>{member.position}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </HStack>
+      </Stack>
     </>
   );
 }
